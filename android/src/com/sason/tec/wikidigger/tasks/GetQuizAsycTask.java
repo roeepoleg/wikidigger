339c7fb3626bbf8b1e88e30abcc373708f0ac898
roee poleg <roee.poleg@gmail.com>
2013-11-23 23:33:46 +0200

    using real server data

--- android/src/com/sason/tec/wikidigger/tasks/GetQuizAsycTask.java
+++ android/src/com/sason/tec/wikidigger/tasks/GetQuizAsycTask.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.sason.tec.wikidigger.vo.QuizVO;
@@ -20,7 +22,7 @@ public class GetQuizAsycTask extends AsyncTask<String, Void, QuizVO> {
 	private static final String jsonDummy = "{\"answers\":[{\"url\":\"http://en.wikipedia.org/wiki/Taxonomy_(biology)\",\"title\":\"Taxonomy_(biology)\",\"distance\":2,\"isCorrect\":true},{\"url\":\"http://en.wikipedia.org/wiki/Carl_Linnaeus\",\"title\":\"Carl_Linnaeus\",\"distance\":3,\"isCorrect\":false},{\"url\":\"http://en.wikipedia.org/wiki/Provinces_of_Sweden\",\"title\":\"Provinces_of_Sweden\",\"distance\":6,\"isCorrect\":false},{\"url\":\"http://en.wikipedia.org/wiki/Petty_kingdom\",\"title\":\"Petty_kingdom\",\"distance\":9,\"isCorrect\":false}],\"question\":{\"url\":\"http://en.wikipedia.org/wiki/Lambeosaurus\",\"title\":\"Lambeosaurus\",\"distance\":0,\"isCorrect\":false}}";
 	//{"answers":[{"url":"http://en.wikipedia.org/wiki/Taxonomy_(biology)","title":"Taxonomy_(biology)","distance":2},{"url":"http://en.wikipedia.org/wiki/Carl_Linnaeus","title":"Carl_Linnaeus","distance":3},{"url":"http://en.wikipedia.org/wiki/Provinces_of_Sweden","title":"Provinces_of_Sweden","distance":6},{"url":"http://en.wikipedia.org/wiki/Petty_kingdom","title":"Petty_kingdom","distance":9}],"question":{"url":"http://en.wikipedia.org/wiki/Lambeosaurus","title":"Lambeosaurus","distance":0}}
 
-	private static final String REST_API_PATH = "http://secure-spire-2766.herokuapp.com/quiz";
+	private static final String REST_API_PATH = "http://secure-spire-2766.herokuapp.com/quiz/";
 
 	public IGetQuizTaskHandler handler;
 
@@ -53,7 +55,7 @@ public class GetQuizAsycTask extends AsyncTask<String, Void, QuizVO> {
 
 			// Execute the request
 			HttpResponse response;
-			/*try {
+			try {
 	        response = httpclient.execute(httpget);
 	        // Examine the response status
 	        Log.i("GetQuizAsycTask",response.getStatusLine().toString());
@@ -77,9 +79,9 @@ public class GetQuizAsycTask extends AsyncTask<String, Void, QuizVO> {
 
 
 	    } catch (Exception e) {}
-			 */
+			 
 		}
-		quiz = (new Gson()).fromJson(jsonDummy, QuizVO.class);
+	//	quiz = (new Gson()).fromJson(jsonDummy, QuizVO.class);
 		return quiz;
 	}
 
